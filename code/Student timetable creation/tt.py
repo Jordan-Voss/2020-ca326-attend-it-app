1d0e059a04536e7051e6550f02960fe9433ac9c4
vossj2 <jordan.voss2@mail.dcu.ie>
2020-02-06 15:31:59 +0000

    Added module input

--- code/Student timetable creation/tt.py
+++ code/Student timetable creation/tt.py
@@ -4,6 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 modules = []
 
 class ModuleGrid(GridLayout):
