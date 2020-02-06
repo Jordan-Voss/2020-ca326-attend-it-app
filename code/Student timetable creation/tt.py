078f66ba6d140328e55da6ef4c27e84092409b51
vossj2 <jordan.voss2@mail.dcu.ie>
2020-02-06 17:16:29 +0000

    Changed module attributes to class, added .kv file

--- code/Student timetable creation/tt.py
+++ code/Student timetable creation/tt.py
@@ -5,48 +5,22 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-modules = []
+from kivy.properties import ObjectProperty
 
-class ModuleGrid(GridLayout):
-    def __init__(self,**kwargs):
-        super(ModuleGrid,self).__init__(**kwargs)
-        self.inside = GridLayout()
-        self.inside.cols = 3
-        self.cols = 1
-        self.inside.orientation = 'vertical'
-
-        self.inside.add_widget(Label(text="Module Name: ",size_hint = (.5, .25)))
-        self.name = TextInput(multiline=False,size_hint = (.5, 1))
-        self.inside.add_widget(self.name)
-        self.inside.add_widget(Label(size_hint = (.5, .25)))
-
-        self.inside.add_widget(Label(text="Module Start Time: ",size_hint = (.5, .25)))
-        self.time = TextInput(multiline=False,size_hint = (.5, 1))
-        self.inside.add_widget(self.time)
-        self.inside.add_widget(Label(size_hint = (.5, .25)))
-
-        self.inside.add_widget(Label(text="Module Duration: ",size_hint = (.5, .25)))
-        self.dur = TextInput(multiline=False,size_hint = (.5, 1))
-        self.inside.add_widget(self.dur)
-        self.inside.add_widget(Label(size_hint = (.5, .25)))
-
-        self.inside.add_widget(Label(text="Module Location: ",size_hint = (.5, .25)))
-        self.loc = TextInput(multiline=False,size_hint = (.5, 1))
-        self.inside.add_widget(self.loc)
-        self.inside.add_widget(Label(size_hint = (.5, .25)))
-
-        self.add_widget(self.inside)
-
-        self.inside.add_widget(Label(size_hint = (1, 1.5)))
-        self.submit_module = Button(text="Add Module", size_hint = (1,.5),
-        pos_hint = {'center_x': .5, 'center_y':.5})
-        self.submit_module.bind(on_press=self.pressed)
-        self.inside.add_widget(self.submit_module)
-        self.inside.add_widget(Label(size_hint = (1, 1.5)))
-
-
-        self.add_widget(Label(size_hint = (1, 1)))
-    def pressed(self,instance):
+class Module():
+    def __init__(self,name,time,duration,location):
+        self.name = name
+        self.time = time
+        self.dur = duration
+        self.loc = location
+    def show(mod):
+        return mod.name,mod.time,mod.dur,mod.loc
+class ModuleGrid(Widget):
+    name = ObjectProperty(None)
+    time = ObjectProperty(None)
+    dur = ObjectProperty(None)
+    loc = ObjectProperty(None)
+    def pressed(self):
         m_name = self.name.text
         self.name.text = ""
         m_time = self.time.text
@@ -55,12 +29,13 @@ class ModuleGrid(GridLayout):
         self.dur.text = ""
         m_location = self.loc.text
         self.loc.text = ""
-        modules.append(m_name)
-        modules.append(m_time)
-        modules.append(m_duration)
-        modules.append(m_location)
-        print(modules)
-
+        #modules.append(m_name)
+        #modules.append(m_time)
+        ##modules.append(m_duration)
+        #modules.append(m_location)
+        #print(modules)
+        mod = Module(m_name,m_time,m_duration,m_location)
+        print(mod.show())
 
 
 class TimetableApp(App):
