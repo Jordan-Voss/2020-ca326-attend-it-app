4d2861bf1d5c73588f4cdb00358bd858caa9ce0b
vossj2 <jordan.voss2@mail.dcu.ie>
2020-02-24 22:37:54 +0000

    Fixed updating database

--- code/student/student.py
+++ code/student/student.py
@@ -5,27 +5,48 @@ from kivy.uix.spinner import Spinner
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty,ListProperty
 from kivy.uix.floatlayout import FloatLayout
-
 import pathlib
 import csv
 import mysql.connector
+
 modules =[]
+
 def add_mod_db(id,mod_name):
     print(id,mod_name)
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Jmgv1234",
+    passwd="Attendit",
     database="attendit"
     )
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO student (id, mod_name) VALUES (%s, %s)"
+    query = "INSERT IGNORE INTO student (id, mod_name) VALUES (%s, %s)"
     for m in mod_name:
         val = (0,m)
-        mycursor.execute(sql, val)
+        try:
+            mycursor.execute(query, val)
+            mydb.commit()
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+def add_details_db(mod,day,time,location):
+    print(id,mod)
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="Attendit",
+    database="attendit"
+    )
+    mycursor = mydb.cursor()
 
-    mydb.commit()
+    query = "UPDATE student SET day = %s, time = %s, location = %s  WHERE mod_name = %s ;"
+    val = (day, time, location, mod)
+    try:
+        mycursor.execute(query,val)
+        mydb.commit()
+    except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        App.get_running_app().stop()
 
 class LoginWindow(Screen):
     pass
@@ -45,29 +66,35 @@ class MainWindow(Screen):
         if self.nam.text != '':
             m_name = self.nam.text
             self.nam.text = ""
-            modules.append(m_name)
+            if m_name not in modules:
+                modules.append(m_name)
         print(modules)
     def pr(self):
         add_mod_db(1,modules)
 
 class SecondWindow(Screen):
+
     def spinner_clicked(self,text):
         print(text)
-    def mod_but_press(self,mod,time):
+
+    def mod_but_press(self,mod,day,time,loc):
         m_loc=self.loc.text
         self.loc.text=''
-        print(mod,time,m_loc)
+        print(mod,day,time,loc)
+        add_details_db(mod,day,time,loc)
+
     def modules(self):
         print(modules)
+
     def __init__(self, **kwargs):
         self.buildLists()
         super(SecondWindow, self).__init__(**kwargs)
 
     def buildLists(self):
-        self.pickType = ['Select','#1','#2','#3']
+        self.pickType = ['Select',]
         self.pickSubType = ['Select']
 
-    def updateSubSpinner(self, text):
+    def updateSpinner(self, text):
 
         if text == 'Select':
             self.ids.spinner_1.values = modules
