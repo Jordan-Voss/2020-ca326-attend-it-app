add13e871887dd19ee9e01eb2add2773f4ef31f3
vossj2 <jordan.voss2@mail.dcu.ie>
2020-02-27 17:29:25 +0000

    Added module rankings page and fixed database communication

--- code/student/student.py
+++ code/student/student.py
@@ -5,14 +5,11 @@ from kivy.uix.spinner import Spinner
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty,ListProperty
 from kivy.uix.floatlayout import FloatLayout
-import pathlib
-import csv
 import mysql.connector
 
 modules =[]
 
 def add_mod_db(id,mod_name):
-    print(id,mod_name)
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -21,7 +18,7 @@ def add_mod_db(id,mod_name):
     )
     mycursor = mydb.cursor()
 
-    query = "INSERT IGNORE INTO student (id, mod_name) VALUES (%s, %s)"
+    query = "INSERT INTO student (id, mod_name) VALUES (%s, %s)"
     for m in mod_name:
         val = (0,m)
         try:
@@ -29,8 +26,9 @@ def add_mod_db(id,mod_name):
             mydb.commit()
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
+            App.get_running_app().stop()
 def add_details_db(mod,day,time,location):
-    print(id,mod)
+
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -38,9 +36,9 @@ def add_details_db(mod,day,time,location):
     database="attendit"
     )
     mycursor = mydb.cursor()
-
-    query = "UPDATE student SET day = %s, time = %s, location = %s  WHERE mod_name = %s ;"
-    val = (day, time, location, mod)
+    id =0
+    query = "INSERT INTO student(id,mod_name,day,time,location) VALUES (%s,%s,%s,%s,%s);"
+    val = (0, mod,day, time, location)
     try:
         mycursor.execute(query,val)
         mydb.commit()
@@ -50,6 +48,7 @@ def add_details_db(mod,day,time,location):
 
 class LoginWindow(Screen):
     pass
+
 
 class MainWindow(Screen):
     nam = ObjectProperty(None)
@@ -68,9 +67,11 @@ class MainWindow(Screen):
             self.nam.text = ""
             if m_name not in modules:
                 modules.append(m_name)
-        print(modules)
     def pr(self):
-        add_mod_db(1,modules)
+        #add_mod_db(1,modules)
+        global m
+        m = modules[:]
+
 
 class SecondWindow(Screen):
 
@@ -80,11 +81,7 @@ class SecondWindow(Screen):
     def mod_but_press(self,mod,day,time,loc):
         m_loc=self.loc.text
         self.loc.text=''
-        print(mod,day,time,loc)
         add_details_db(mod,day,time,loc)
-
-    def modules(self):
-        print(modules)
 
     def __init__(self, **kwargs):
         self.buildLists()
@@ -98,6 +95,118 @@ class SecondWindow(Screen):
 
         if text == 'Select':
             self.ids.spinner_1.values = modules
+class ThirdWindow(Screen):
+    def prs(self):
+        if self.hours.text.isdigit() and 0 < int(self.hours.text) <= 40:
+            global s_hours,r_1,r_2,r_3,r_4,r_5,r_6
+            s_hours = int(self.hours.text)
+            r_1 = self.rnk_spin_1.text
+            r_2 = self.rnk_spin_2.text
+            r_3 = self.rnk_spin_3.text
+            r_4 = self.rnk_spin_4.text
+            r_5 = self.rnk_spin_5.text
+            r_6 = self.rnk_spin_6.text
+            self.manager.current = 'study'
+
+        else:
+            pass
+        self.hours.text =''
+
+    def __init__(self, **kwargs):
+        self.buildLists2()
+        m = modules[:]
+        super(ThirdWindow, self).__init__(**kwargs)
+
+    def buildLists2(self):
+        self.pickType = ['Rank']
+        self.pickSubType = ['Select']
+
+    def updateSpinner2(self, text):
+        global m
+        if text == 'Rank':
+            self.ids.rnk_spin_1.values = m
+            self.ids.rnk_spin_2.values = m
+            self.ids.rnk_spin_3.values = m
+            self.ids.rnk_spin_4.values = m
+            self.ids.rnk_spin_5.values = m
+            self.ids.rnk_spin_6.values = m
+
+        else:
+            i = 0
+            while i < len(m):
+
+                self.ids.rnk_spin_2.values = m
+                self.ids.rnk_spin_3.values = m
+                self.ids.rnk_spin_4.values = m
+                self.ids.rnk_spin_5.values = m
+                self.ids.rnk_spin_6.values = m
+                self.ids.rnk_spin_1.values = m
+                #n = modules[:i] + modules[i+1:]
+                if text == m[i]:
+                    m.pop(i)
+                    self.ids.rnk_spin_2.values = m
+                    self.ids.rnk_spin_3.values = m
+                    self.ids.rnk_spin_4.values = m
+                    self.ids.rnk_spin_5.values = m
+                    self.ids.rnk_spin_6.values = m
+                    self.ids.rnk_spin_1.values = m
+                i += 1
+    def reset(self,rnk_spin_1,rnk_spin_2,rnk_spin_3,rnk_spin_4,rnk_spin_5,rnk_spin_6):
+        global m
+        m = modules[:]
+        self.rnk_spin_1.text = '1'
+        self.rnk_spin_2.text = '2'
+        self.rnk_spin_3.text = '3'
+        self.rnk_spin_4.text = '4'
+        self.rnk_spin_5.text = '5'
+        self.rnk_spin_6.text = '6'
+class StudyTTWindow(Screen):
+    def add_study_db():
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="Attendit",
+        database="attendit"
+        )
+        mycursor = mydb.cursor(buffered=True)
+
+        try:
+            mycursor.execute("INSERT INTO student (id,mod_name,day,time)SELECT * FROM (SELECT 0,'a','Wednesday','6h') AS` VALUES`WHERE NOT EXISTS ( SELECT id,day FROM student WHERE mod_name='a' AND day = 'Wednesday' AND time = '6h') LIMIT 1;")
+            mydb.commit()
+            number_of_rows= mycursor.execute("SELECT * FROM student")
+            rows = mycursor.fetchall()
+            for row in rows:
+                print(row)
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            App.get_running_app().stop()
+        #else:
+        #    print(row_count)
+    def ranks(self):
+        global modules
+        ranking = {}
+        ranking[r_1] = 6
+        ranking[r_2] = 5
+        ranking[r_3] = 4
+        ranking[r_4] = 3
+        ranking[r_5] = 2
+        ranking[r_6] = 1
+        m = 0
+        for item in ranking:
+            m= m + ranking[item]
+        mod_hours = {}
+        rnk = 6
+        for r in modules:
+            if ranking[r] == rnk:
+                mod_hours[r] = rnk/s_hours
+                rnk = rnk - 1
+
+        am = []
+        for i in mod_hours:
+            am.append(float(mod_hours[i]))
+
+        StudyTTWindow.add_study_db()
+
 
 
 class WindowManager(ScreenManager):
